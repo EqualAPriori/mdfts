fd6f86f4ff1d2dbd1fed89bb59608d84cb81ceeb
Charles Li <charles9000li@gmail.com>
2022-01-21 22:25:39 -0800

    bead types and potentials now in separate object

--- src/mdfts/forcefield/forcefield.py
+++ src/mdfts/forcefield/forcefield.py
@@ -12,19 +12,21 @@ class BeadType(object):
         self.name = name
         self.smear_length = smear_length
         self.charge = charge
+
     def __str__(self):
         return self.to_dict()
+
     def __repr__(self):
         return 'BeadType: {}'.format(self.to_dict())
 
 
-@serial.serialize(['kT', '_bead_types', '_potentials'])
+@serial.serialize(['kT', 'bead_types', 'potentials'])
 class ForceField(object):
 
     def __init__(self, kT=1.0):
         self.kT = kT
-        self._bead_types = []
-        self._potentials = []
+        self._bead_types = _BeadTypes()
+        self._potentials = _Potentials()
 
     @property
     def kT(self):
@@ -43,23 +45,22 @@ class ForceField(object):
 
     @property
     def bead_names(self):
-        return [bt.name for bt in self.bead_types]
+        return self._bead_types.bead_names()
 
     def add_bead_type(self, bead_type):
-        if bead_type.name in self.bead_names:
-            raise ValueError("ForceField instance already contains BeadType '{}'".format(bead_type.name))
-        self._bead_types.append(bead_type)
+        self._bead_types.add_bead_type(bead_type)
 
     def get_bead_type(self, bead_name):
-        for bt in self.bead_types:
-            if bead_name == bt.name:
-                return bt
-        raise ValueError("ForceField instance does not contain BeadType '{}'".format(bead_name))
+        return self._bead_types.get_bead_type(bead_name)
 
     def reorder_bead_types(self, bead_names):
         if set(bead_names) != set(self.bead_names):
             raise ValueError("provided bead names don't match bead names in ForceField instance")
         self._bead_types = [self.get_bead_type(bn) for bn in bead_names]
+
+    @property
+    def potentials(self):
+        return self._potentials
 
     # More transparent printing for debugging. I.e. make sure bead types are getting instantiated
     def __str__(self):
@@ -68,33 +69,63 @@ class ForceField(object):
                 [bt.__str__() for bt in self._bead_types])
         ret += "Potentials: {}".format([p.__str__() for p in self._potentials])
         return ret
-        #return self.to_dict()
+        # return self.to_dict()
 
     # Need customized-handling for _bead_types and _potentials to iterate through
     # alternative is to do what `sim` does: define yet another `bead_types(list)`
     # object that extends list, and put the custom get/set in that new object
     # in the future, can further modify these functions to allow support for alternative representations
-    def custom_get(self,k):
+    def custom_get(self, k):
         if k == '_bead_types':
             return [p.to_dict() for p in self._bead_types]
         elif k == '_potentials':
             return [p.to_dict() for p in self._potentials]
         else:
-            return serial.Serializable.custom_get(self,k)  
+            return serial.Serializable.custom_get(self, k)
 
-    def custom_set(self,k,v):
+    def custom_set(self, k, v):
         """right now completely over-writes _bead_types and _potentials.
              
         maybe want to raise some kind of error if bead types don't match?
         """
-        if k == '_bead_types': 
+        if k == '_bead_types':
             self._bead_types = []
-            for bead_def in v: #assume `v` is a `list` of `bead_type` dictionaries
-                self._bead_types.append( BeadType(**bead_def) )
+            for bead_def in v:  # assume `v` is a `list` of `bead_type` dictionaries
+                self._bead_types.append(BeadType(**bead_def))
         elif k == '_potentials':
             self._potentials = []
-            for potential_def in v: #assume `v` is a `list` of `` dictionaries
-                pass #Implement once potentials are implemented
-                #perhaps looks like _POTENTIALTYPES[potential_name](**otherargs)
+            for potential_def in v:     # assume `v` is a `list` of `` dictionaries
+                pass # Implement once potentials are implemented
+                # perhaps looks like _POTENTIALTYPES[potential_name](**otherargs)
+        else:
+            self.custom_set(k, v)
         
 
+@serial.serialize([])
+class _BeadTypes(list):
+
+    def __init__(self):
+        super(_BeadTypes, self).__init__()
+
+    def add_bead_type(self, bead_type):
+        if not isinstance(bead_type, BeadType):
+            raise TypeError("bead_type argument must be of type mdfts.forcefield.BeadType")
+        if bead_type.name in self.bead_names:
+            raise ValueError("BeadType '{}' already exists in ForceField instance".format(bead_type.name))
+        self.append(bead_type)
+
+    def get_bead_type(self, bead_name):
+        for bt in self:
+            if bead_name == bt.name:
+                return bt
+        raise ValueError("ForceField instance does not contain BeadType '{}'".format(bead_name))
+
+    def bead_names(self):
+        return [bt.name for bt in self]
+
+
+@serial.serialize([])
+class _Potentials(list):
+
+    def __init__(self):
+        super(_Potentials, self).__init__()
